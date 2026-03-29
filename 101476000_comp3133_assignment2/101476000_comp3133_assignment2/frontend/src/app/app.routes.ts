192fe404d5a7b7516c94ddcabf9915414f89308c
Carlos Figuera La Riva <101476000@georgebrown.ca>
2026-03-29 19:25:50 -0400

    Update app.routes.ts

--- 101476000_comp3133_assignment2/101476000_comp3133_assignment2/frontend/src/app/app.routes.ts
+++ 101476000_comp3133_assignment2/101476000_comp3133_assignment2/frontend/src/app/app.routes.ts
@@ -1,12 +1,11 @@
-import { Routes } from '@angular/router';
+import { Routes, CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
 import { LoginComponent } from './features/auth/login/login.component';
 import { SignupComponent } from './features/auth/signup/signup.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { EmployeeListComponent } from './features/employee/employee-list/employee-list.component';
 import { EmployeeFormComponent } from './features/employee/employee-form/employee-form.component';
 import { AuthService } from './services/auth.service';
-import { inject } from '@angular/core';
-import { Router, CanActivateFn } from '@angular/router';
 
 export const authGuard: CanActivateFn = (_, state) => {
   const authService = inject(AuthService);
